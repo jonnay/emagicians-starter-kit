c21dcbafbd330a9d10166d88c42dcf1c6a069dff
jonnay <jonnay@jonnay.net>
2013-06-14 09:12:38 -0600

    Delete lisp files taht are auto-generated.

--- darwin.el
+++
@@ -1,8 +0,0 @@
-
-(when (fboundp 'ns-list-colors)
-  (setq x-colors (ns-list-colors)))
-
-(setq org-latex-to-pdf-process 
-      '("/usr/texbin/pdflatex -interaction nonstopmode -output-directory %o %f" 
-        "/usr/texbin/pdflatex -interaction nonstopmode -output-directory %o %f" 
-        "/usr/texbin/pdflatex -interaction nonstopmode -output-directory %o %f"))
